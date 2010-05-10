58bb93929c362ec6d254f2cf75be8711c4d1557e
Kees Trippelvitz <github@nabber.nl>
2010-05-10 15:23:32 +0000

    Added log-janitor.pl to cron.
    git-svn-id: https://svn.surfnet.nl/svn/ids/packaging/logserver/trunk@1601 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- debian/cron.d
+++ debian/cron.d
@@ -3,4 +3,5 @@
 #
 25 6 * * * root /opt/surfnetids/scripts/getsandbox.pl >/dev/null
 1 * * * * root /opt/surfnetids/scripts/mailreporter.pl >/dev/null
+50 4 * * * root /opt/surfnetids/scripts/log-janitor.pl >/dev/null
 
