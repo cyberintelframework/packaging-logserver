c33b114e655ab0ce3e1154fbe9119a518ae805df
Kees Trippelvitz <github@nabber.nl>
2010-06-11 13:48:47 +0000

    Updated make_release.
    git-svn-id: https://svn.surfnet.nl/svn/ids/packaging/logserver/tags/3.0rc1@1634 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- debian/cron.d
+++ debian/cron.d
@@ -2,5 +2,5 @@
 # Regular cron jobs for the surfids-logserver package
 #
 25 6 * * * root /opt/surfnetids/scripts/getsandbox.pl >/dev/null
-1 * * * * root /opt/surfnetids/scripts/mailreporter.pl >/dev/null
+*/15 * * * * root /opt/surfnetids/scripts/mailreporter.pl >/dev/null
 
