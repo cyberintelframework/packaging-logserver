5efe583cd91d2f8a6e79ffb95c7ce5e29790853e
Kees Trippelvitz <github@nabber.nl>
2010-12-06 15:51:23 +0000

    Fixed crontab entry for mailreports.
    git-svn-id: https://svn.surfnet.nl/svn/ids/packaging/logserver/trunk@1722 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- debian/cron.d
+++ debian/cron.d
@@ -2,6 +2,6 @@
 # Regular cron jobs for the surfids-logserver package
 #
 25 6 * * * root /opt/surfnetids/scripts/getsandbox.pl >/dev/null
-1 * * * * root /opt/surfnetids/scripts/mailreporter.pl >/dev/null
+*/5 * * * * root /opt/surfnetids/scripts/mailreporter.pl >/dev/null
 50 4 * * * root /opt/surfnetids/scripts/log-janitor.pl >/dev/null
 
